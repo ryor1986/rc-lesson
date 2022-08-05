70723fc8ae2ab22658759a8ba551d15b12518290
ryota <ryor.com@gmail.com>
2022-08-05 13:23:01 +0900

    授業用ソースを追加

---
+++ samplePage/js/function.js
@@ -0,0 +1,13 @@
+// ログイン処理
+function login() {
+    alert('ログインします！');
+}
+
+/*
+　　計算結果を表示する関数
+  　1+1ボタンから呼び出されます
+*/
+function calc() {
+    let result = 1 + 1;
+    console.log(result); //計算結果を表示
+}
