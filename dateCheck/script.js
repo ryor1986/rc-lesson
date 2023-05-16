2f7e1152c8bfec230bb8696028d83f252cc9fdb7
ryota <ryor.com@gmail.com>
2023-05-16 18:00:00 +0900

    日付チェックの課題を追加

---
+++ dateCheck/script.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function() {
+    let button = document.getElementById('submit');
+    button.addEventListener('click', function() {
+        // ここに処理を書く
+    });
+});
